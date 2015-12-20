0c76b49f6e0e720ffe47a88d36be0da1a8d9b3ce
Shenya <88459122@qq.com>
2015-12-20 19:59:17 +0800

    Create sy.c

---
+++ sy.c
@@ -0,0 +1,13 @@
+//作者:沈雅
+//时间:2015-12-20
+//版权:GPLv2
+//用于实验室数据传感器读取index.html
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main(void)
+{
+    printf("#15#20#15#15#15#");
+}
